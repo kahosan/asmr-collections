8a8cf46df61879c2e544dd9495ad0a70491fc9d9
kahosan <15116355657l@gmail.com>
2025-11-11 19:10:33 +0800

    feat(server): support cloudflare

---
+++ apps/server/cloudflare/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { api } from '../src/router/api';
+import { proxyApp } from '../src/router/proxy';
+
+interface Bindings {
+  ASSETS: {
+    fetch: typeof fetch
+  }
+}
+
+export const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+  '*',
+  logger((message, ...rest) => {
+    return console.info(message, ...rest);
+  })
+);
+
+app.route('/api', api);
+app.route('/proxy', proxyApp);
+
+// web
+app.get('/work-details/*', c => {
+  return c.env.ASSETS.fetch(new URL('/index.html', c.req.url));
+});
+
+export default {
+  fetch: app.fetch,
+  idleTimeout: 60
+};
--- apps/server/src/lib/db.ts
+++ apps/server/src/lib/db.ts
@@ -1,7 +1,13 @@
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export function getPrisma() {
+  if (process.env.RUNTIME === 'workers') {
+    const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+    return new PrismaClient({ adapter });
+  }
+
   return prisma;
 }
