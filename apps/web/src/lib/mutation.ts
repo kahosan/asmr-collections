a322003727608a58ef4c94d2c003360494a8d1b2
kahosan <15116355657l@gmail.com>
2025-11-11 21:47:28 +0800

    fix(web): preload mutate

--- apps/web/src/lib/mutation.ts
+++ apps/web/src/lib/mutation.ts
@@ -1,7 +1,30 @@
 import { mutate } from 'swr';
+import { fetcher, HTTPError } from './fetcher';
+
+import { logger } from './logger';
+import { notifyError } from './utils';
 
 export function mutateWorkInfo(id: string) {
-  mutate(`work-info-${id}`);
+  mutate(`work-info-${id}`, async () => {
+    try {
+      return await fetcher(`/api/work/${id}`);
+    } catch (e) {
+      if (e instanceof HTTPError && e.status === 404) {
+        try {
+          logger.warn(`${id} 不存在于数据库中，尝试使用 DLsite 预加载作品信息`);
+          const data = await fetcher<Record<string, string>>(`/api/work/info/${id}`);
+          return { ...data, exists: false };
+        } catch (e) {
+          notifyError(e, '获取作品信息失败');
+          logger.error(e, '预加载作品信息失败');
+          return null;
+        }
+      }
+      notifyError(e, '获取作品信息失败');
+      logger.error(e, '预加载作品信息失败');
+      return null;
+    }
+  }, { revalidate: false });
 }
 
 export function mutateWorks() {
