1ee93d5e9e22633999f4c7e987e0b5a722aacb8f
kahosan <15116355657l@gmail.com>
2025-11-16 11:32:29 +0800

    chore(web): u

--- apps/web/src/components/media-player/components/right-controls/index.tsx
+++ apps/web/src/components/media-player/components/right-controls/index.tsx
@@ -27,10 +27,10 @@ export default function RightControls() {
       <div className="flex items-center gap-4 group/volume">
         <div className="min-w-20 opacity-0 transition-opacity duration-200 group-hover/volume:opacity-100">
           <VolumeSlider.Root className="group relative mx-[7.5px] h-1 w-full cursor-pointer touch-none select-none items-center outline-none aria-hidden:hidden before:absolute before:inset-0 before:-top-2 before:-bottom-2 before:content-['']">
-            <VolumeSlider.Track className="relative ring-white z-0 h-1 w-full rounded-sm bg-white/30 group-data-[focus]:ring-[1px]">
-              <VolumeSlider.TrackFill className="bg-white/70 absolute h-full w-[var(--slider-fill)] rounded-sm will-change-[width]" />
+            <VolumeSlider.Track className="relative ring-white z-0 h-1 w-full rounded-sm bg-foreground/20 group-data-focus:ring-[1px]">
+              <VolumeSlider.TrackFill className="bg-blue-400 absolute h-full w-(--slider-fill) rounded-sm will-change-[width]" />
             </VolumeSlider.Track>
-            <VolumeSlider.Thumb className="absolute left-[var(--slider-fill)] top-1/2 z-20 size-4 -translate-x-1/2 -translate-y-1/2 rounded-full border border-[#cacaca] bg-white opacity-0 ring-white/40 transition-opacity group-data-[active]:opacity-100 group-data-[dragging]:ring-4 will-change-[left]" />
+            <VolumeSlider.Thumb className="absolute left-(--slider-fill) top-1/2 z-20 size-4 -translate-x-1/2 -translate-y-1/2 rounded-full border border-[#cacaca] bg-white opacity-0 ring-white/40 transition-opacity group-data-active:opacity-100 group-data-dragging:ring-4 will-change-[left]" />
           </VolumeSlider.Root>
         </div>
         <div className="rounded-full p-2 dark:hover:bg-white/15 hover:bg-black/15 transition-colors cursor-pointer opacity-80" onClick={handleMuteUnmute}>
