7425747d4861d59f4e20234eb5daa66f198a3b2e
kahosan <15116355657l@gmail.com>
2025-11-22 23:37:17 +0800

    chore(web): remove `onOpenAutoFocus`  for settings

--- apps/web/src/components/header/settings/index.tsx
+++ apps/web/src/components/header/settings/index.tsx
@@ -8,12 +8,7 @@ export default function SettingsDialog({ open, setOpen }: { open: boolean, setOp
       open={open}
       onOpenChange={setOpen}
     >
-      <AlertDialogContent
-        className="rounded-lg max-w-[95%] sm:max-w-lg px-0"
-        onOpenAutoFocus={event => {
-          event.preventDefault();
-        }}
-      >
+      <AlertDialogContent className="rounded-lg max-w-[95%] sm:max-w-lg px-0">
         <SettingsDialogContent key={open ? 'open' : 'close'} setOpen={setOpen} />
       </AlertDialogContent>
     </AlertDialog>
