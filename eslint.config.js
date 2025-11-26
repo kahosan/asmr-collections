5b3393ae486cddcb315e2d720fe042fe679a16e7
kahosan <15116355657l@gmail.com>
2025-11-26 13:46:48 +0800

    chore: eslint happy

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ export default kaho(
     ignores: {
       customGlobs: [
         'apps/server/src/lib/prisma/**/*.ts',
-        'apps/server/src/lib/prisma-workers/**/*.ts',
+        'apps/server/src/lib/prisma-workers/**/*.{ts,js}',
         'apps/web/src/components/ui/*.tsx',
         'apps/web/tailwind.config.js'
       ]
