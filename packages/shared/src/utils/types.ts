5577371ed7b1ae4b11e5533e909461c0b2ed4153
kahosan <15116355657l@gmail.com>
2025-11-27 11:08:04 +0800

    feat(shared): improve `Jsonify`

--- packages/shared/src/utils/types.ts
+++ packages/shared/src/utils/types.ts
@@ -1,7 +1,14 @@
-export type Jsonify<T> = T extends Date
-  ? string
-  : T extends Array<infer U>
-    ? Array<Jsonify<U>>
-    : T extends object
-      ? { [K in keyof T]: Jsonify<T[K]> }
-      : T;
+export type Jsonify<T> = T extends string | number | boolean | null
+  ? T
+  : T extends Date
+    ? string
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type -- Treat functions as non-serializable
+    : T extends Function | undefined
+      ? null // Functions and undefined are not serializable
+      : T extends object
+        ? T extends Array<infer U>
+          ? Array<Jsonify<U>> // Handle arrays
+          : {
+            [K in keyof T as K extends symbol ? never : K]: Jsonify<T[K]>; // Handle objects and recurse
+          }
+        : never;
