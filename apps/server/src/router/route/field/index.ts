a1edd347d3136353c72545c447d1eedf8f96de08
kahosan <15116355657l@gmail.com>
2025-12-10 16:11:26 +0800

    fix(server): filter works is empy

--- apps/server/src/router/route/field/index.ts
+++ apps/server/src/router/route/field/index.ts
@@ -14,7 +14,12 @@ fieldApp.get('/:field', async c => {
   try {
     if (modelFields.has(field)) {
       // @ts-expect-error 我也没有办法啊啊啊啊啊啊啊啊啊
-      const data = await prisma[field].findMany();
+      const data = await prisma[field].findMany({
+        where: {
+          works: { some: {} }
+        },
+        orderBy: { id: 'asc' }
+      });
       return Array.isArray(data.at(0)) ? c.json(data.flat()) : c.json(data);
     }
 
