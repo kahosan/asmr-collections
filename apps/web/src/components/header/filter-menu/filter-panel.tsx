bd7b1dc76a998fd383b54feb2bb54b4d790c8a1c
kahosan <15116355657l@gmail.com>
2025-12-10 15:50:44 +0800

    fix(web): hidden option when filter error

--- apps/web/src/components/header/filter-menu/filter-panel.tsx
+++ apps/web/src/components/header/filter-menu/filter-panel.tsx
@@ -63,41 +63,43 @@ export default function FilterPanel<T extends string | number>({
           <CommandGroup>
             <Loading isLoading={isLoading} className="w-full mx-auto mt-[50%]" />
             <ErrorComp error={error} text={errorText} />
-            <VirtualizedVirtualizer>
-              {
-                filtered.map(({ id, name }) => {
-                  const checked = isCheck({ id, name });
-                  return (
-                    <motion.div
-                      key={id}
-                      layout
-                      initial={{ opacity: checked ? 1 : 0.65 }}
-                      animate={{ opacity: checked ? 1 : 0.65 }}
-                      whileHover={{ opacity: 1 }}
-                    >
-                      <CommandItem value={name} onSelect={() => handleSelect(id)}>
-                        <div className="truncate">{name}</div>
-                        <AnimatePresence>
-                          {checked && (
-                            <motion.div
-                              initial={{ scale: 0, opacity: 0 }}
-                              animate={{ scale: 1, opacity: 1 }}
-                              exit={{ scale: 0, opacity: 0 }}
-                              transition={{ duration: 0.1, ease: 'easeInOut' }}
-                              className="ml-auto"
-                            >
-                              {checked === 'indeterminate'
-                                ? <MinusIcon />
-                                : <CheckIcon />}
-                            </motion.div>
-                          )}
-                        </AnimatePresence>
-                      </CommandItem>
-                    </motion.div>
-                  );
-                })
-              }
-            </VirtualizedVirtualizer>
+            <Activity mode={(!error && !isLoading) ? 'visible' : 'hidden'}>
+              <VirtualizedVirtualizer>
+                {
+                  filtered.map(({ id, name }) => {
+                    const checked = isCheck({ id, name });
+                    return (
+                      <motion.div
+                        key={id}
+                        layout
+                        initial={{ opacity: checked ? 1 : 0.65 }}
+                        animate={{ opacity: checked ? 1 : 0.65 }}
+                        whileHover={{ opacity: 1 }}
+                      >
+                        <CommandItem value={name} onSelect={() => handleSelect(id)}>
+                          <div className="truncate">{name}</div>
+                          <AnimatePresence>
+                            {checked && (
+                              <motion.div
+                                initial={{ scale: 0, opacity: 0 }}
+                                animate={{ scale: 1, opacity: 1 }}
+                                exit={{ scale: 0, opacity: 0 }}
+                                transition={{ duration: 0.1, ease: 'easeInOut' }}
+                                className="ml-auto"
+                              >
+                                {checked === 'indeterminate'
+                                  ? <MinusIcon />
+                                  : <CheckIcon />}
+                              </motion.div>
+                            )}
+                          </AnimatePresence>
+                        </CommandItem>
+                      </motion.div>
+                    );
+                  })
+                }
+              </VirtualizedVirtualizer>
+            </Activity>
           </CommandGroup>
         </CommandList>
       </Virtualized>
@@ -108,7 +110,7 @@ export default function FilterPanel<T extends string | number>({
 function ErrorComp({ error, text }: { error: unknown, text: string }) {
   if (!error) return null;
   return (
-    <div className="w-full mx-auto mt-[10%] text-center text-sm opacity-80">
+    <div className="w-full mx-auto mt-[45%] text-center text-sm opacity-80">
       {text}
       {error instanceof HTTPError && <div className="text-xs opacity-70">{error.message}</div>}
     </div>
