7cbc85c3b32d2a1f95fda2d069f70725c3e8a70b
kahosan <15116355657l@gmail.com>
2026-08-31 12:48:37 +0800

    fix(web): dedupe react

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -19,7 +19,8 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '~': path.resolve(__dirname, './src')
-      }
+      },
+      dedupe: ['react', 'react-dom']
     },
     server: {
       proxy: {
